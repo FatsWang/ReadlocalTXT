3423d3c1aa5f5b924743139f9ceeb876cf21d40f
FatsWang <57435874+FatsWang@users.noreply.github.com>
2022-06-29 17:40:11 +0800

    Add files via upload

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/ReadlocalTXT/ReadlocalTXT.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeReadlocalTXT_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_ReadlocalTXT;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_ReadlocalTXT()
+	{
+		if (!Z_Registration_Info_UPackage__Script_ReadlocalTXT.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/ReadlocalTXT",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x86A0BD8A,
+				0x451EC0DC,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ReadlocalTXT.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_ReadlocalTXT.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ReadlocalTXT(Z_Construct_UPackage__Script_ReadlocalTXT, TEXT("/Script/ReadlocalTXT"), Z_Registration_Info_UPackage__Script_ReadlocalTXT, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x86A0BD8A, 0x451EC0DC));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
