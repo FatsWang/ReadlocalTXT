9ed385fd2afe29ee2dcb9f5c17c8fd8d2680bd49
FatsWang <57435874+FatsWang@users.noreply.github.com>
2022-06-29 17:46:03 +0800

    Add files via upload

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/ReadlocalTXT/ReadlocalTXTBPLibrary.generated.h
@@ -0,0 +1,111 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef READLOCALTXT_ReadlocalTXTBPLibrary_generated_h
+#error "ReadlocalTXTBPLibrary.generated.h already included, missing '#pragma once' in ReadlocalTXTBPLibrary.h"
+#endif
+#define READLOCALTXT_ReadlocalTXTBPLibrary_generated_h
+
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_SPARSE_DATA
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execRemoveTxt); \
+	DECLARE_FUNCTION(execWriteTxt); \
+	DECLARE_FUNCTION(execReadTxt); \
+	DECLARE_FUNCTION(execISvalidTxt); \
+	DECLARE_FUNCTION(execCreateTxt);
+
+
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execRemoveTxt); \
+	DECLARE_FUNCTION(execWriteTxt); \
+	DECLARE_FUNCTION(execReadTxt); \
+	DECLARE_FUNCTION(execISvalidTxt); \
+	DECLARE_FUNCTION(execCreateTxt);
+
+
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUReadlocalTXTBPLibrary(); \
+	friend struct Z_Construct_UClass_UReadlocalTXTBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UReadlocalTXTBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/ReadlocalTXT"), NO_API) \
+	DECLARE_SERIALIZER(UReadlocalTXTBPLibrary)
+
+
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_INCLASS \
+private: \
+	static void StaticRegisterNativesUReadlocalTXTBPLibrary(); \
+	friend struct Z_Construct_UClass_UReadlocalTXTBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UReadlocalTXTBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/ReadlocalTXT"), NO_API) \
+	DECLARE_SERIALIZER(UReadlocalTXTBPLibrary)
+
+
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UReadlocalTXTBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UReadlocalTXTBPLibrary) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UReadlocalTXTBPLibrary); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UReadlocalTXTBPLibrary); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UReadlocalTXTBPLibrary(UReadlocalTXTBPLibrary&&); \
+	NO_API UReadlocalTXTBPLibrary(const UReadlocalTXTBPLibrary&); \
+public:
+
+
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UReadlocalTXTBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UReadlocalTXTBPLibrary(UReadlocalTXTBPLibrary&&); \
+	NO_API UReadlocalTXTBPLibrary(const UReadlocalTXTBPLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UReadlocalTXTBPLibrary); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UReadlocalTXTBPLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UReadlocalTXTBPLibrary)
+
+
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_PRIVATE_PROPERTY_OFFSET
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_11_PROLOG
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_PRIVATE_PROPERTY_OFFSET \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_SPARSE_DATA \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_RPC_WRAPPERS \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_INCLASS \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_PRIVATE_PROPERTY_OFFSET \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_SPARSE_DATA \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_INCLASS_NO_PURE_DECLS \
+	RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h_14_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class ReadlocalTXTBPLibrary."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> READLOCALTXT_API UClass* StaticClass<class UReadlocalTXTBPLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID RemoveTxt_Plugins_ReadlocalTXT_Source_ReadlocalTXT_Public_ReadlocalTXTBPLibrary_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
